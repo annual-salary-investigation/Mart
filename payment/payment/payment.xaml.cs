5a03f22f9fab02decc9880a4c523e73adbec6cca
K-ji-eun <snstkfkawldm@naver.com>
2023-05-08 17:05:58 +0900

    payment
    결제창

---
+++ payment/payment/payment.xaml.cs
@@ -0,0 +1,43 @@
+﻿using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace payment
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class payment : MetroWindow
+    {
+        public payment()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnNo_Click(object sender, RoutedEventArgs e)
+        {   
+            // 장바구니로 돌아가기
+            //NavigationService.Navigate(new Uri("/ .xaml", UriKind.RelativeOrAbsolute)); 
+        }
+
+        private void BtnPayment_Click(object sender, RoutedEventArgs e)
+        {
+            // 결제완료 팝업창
+            
+            this.ShowMessageAsync("결제 완료", "결제가 완료되었습니다.");
+        }
+    }
+}
